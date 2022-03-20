cca7c9d19c9318b751b3a4c903998aa715050d28
CamTyrese <camtyrese96@gmail.com>
2022-03-20 16:41:53 -0500

    first commit

---
+++ portfolio/index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+
+    <!--- basic page needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <title>Ethos</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+ <?php require('../includes/head.php');?>
+
+</head>
+
+<body id="top" class="">
+
+
+   <?php require('../includes/preloader.php');?>
+   <?php require('../includes/header.php');?>
+
+
+
+     <!-- portfolio
+    ================================================== -->
+    <section id="portfolio" class="s-portfolio target-section">
+
+        <div class="row s-portfolio__header">
+            <div class="column large-6 medium-8 tab-full">
+                <div class="section-intro" data-num="03" data-aos="fade-up">
+                    <h3 class="subhead">Portfolio</h3>
+                    <h1 class="display-1">
+                        A selection of recent projects I have worked on.
+                    </h1>
+                </div>
+            </div>
+        </div> <!-- s-porfolio__header -->
+
+        <div class="row s-porfolio__list block-large-1-2 block-tab-full collapse">
+
+            <div class="column" data-aos="fade-up">
+                <div class="folio-item">
+                    <div class="folio-item__thumb">
+                        <a class="folio-item__thumb-link" href="https://cameverett96.github.io/Ecommerce-store/" title="Windows in The City" data-size="1050x700">
+                            <img src="/images/portfolio/hypecity.png" 
+                                 srcset="/images/portfolio/hypecity.png" alt="">
+                        </a>
+                    </div>
+                    <div class="folio-item__info">
+                        <div class="folio-item__cat">Ecommerce Theme</div>
+                        <h4 class="folio-item__title">Hypi</h4>
+                    </div>
+                    <a href="https://cameverett96.github.io/Ecommerce-store/" title="Project Link" class="folio-item__project-link">Project Link</a>
+                    <div class="folio-item__caption">
+                        <p>Clothing Store theme built with Html, Css, and Next.Js</p>
+                    </div>
+                </div>
+            </div> <!-- end column -->
+            <div class="column" data-aos="fade-up">
+                <div class="folio-item">
+                    <div class="folio-item__thumb">
+                        <a class="folio-item__thumb-link" href="https://cameverett96.github.io/Newsletter_email/" title="Building Blocks" data-size="1050x700">
+                            <img src="/images/portfolio/newsletteremailsmall.png" 
+                                 srcset="/images/portfolio/newsletteremailsmall.png" alt="">
+                        </a>
+                    </div>
+                    <div class="folio-item__info">
+                        <div class="folio-item__cat">Email Marketing</div>
+                        <h4 class="folio-item__title">Newsletter Email Template</h4>
+                    </div>
+                    <a href="https://cameverett96.github.io/Newsletter_email/" title="Project Link" class="folio-item__project-link">Project Link</a>
+                    <div class="folio-item__caption">
+                        <p>News Email template built with Zurb,Foundation using Html and Sass </p>
+                    </div>
+                </div>
+            </div> <!-- end column -->
+            <div class="column" data-aos="fade-up">
+                <div class="folio-item">
+                    <div class="folio-item__thumb">
+                        <a class="folio-item__thumb-link" href="https://camcodedit.myshopify.com/?_ab=0&_fd=0&_sc=1" Title="Shout and Jump" data-size="1050x700">
+                            <img src="/images/portfolio/shopify.png" 
+                                 srcset="/images/portfolio/shopify.png" alt="">
+                        </a>
+                    </div>
+                    <div class="folio-item__info">
+                        <div class="folio-item__cat">Branding</div>
+                        <h4 class="folio-item__title">Shopify theme</h4>
+                    </div>
+                    <a href="https://camcodedit.myshopify.com/?_ab=0&_fd=0&_sc=1" title="Project Link" class="folio-item__project-link">Project Link</a>
+                    <div class="folio-item__caption">
+                        <p>This is a fully customizable theme I built using html, css , next.js and converted into shopify theme using liquid.</p>
+                    </div>
+                </div>
+            </div> <!-- end column -->
+            <div class="column" data-aos="fade-up">
+                <div class="folio-item">
+                    <div class="folio-item__thumb">
+                        <a class="folio-item__thumb-link" href="https://cameverett96.github.io/react-re/" title="Sand Dunes" data-size="1050x700">
+                            <img src="background: url(img/QPiottB\ -\ Imgur.jpg)" 
+                                 srcset="background: url(img/QPiottB\ -\ Imgur.jpg)" alt="">
+                        </a>
+                    </div>
+                    <div class="folio-item__info">
+                        <div class="folio-item__cat">Marketing Email</div>
+                        <h4 class="folio-item__title">Kayak Email</h4>
+                    </div>
+                    <a href="https://cameverett96.github.io/kayak/" title="Project Link" class="folio-item__project-link">Project Link</a>
+                    <div class="folio-item__caption">
+                        <p>Email made with Foundation using html and css</p>
+                    </div>
+                </div>
+            </div> end column
+            <div class="column" data-aos="fade-up">
+                <div class="folio-item">
+                    <div class="folio-item__thumb">
+                        <a class="folio-item__thumb-link" href="https://cameverett96.github.io/EcommerceTheme/" title="Sand Dunes" data-size="1050x700">
+                            <img src="/images/portfolio/woodcraft.jpg" 
+                                 srcset="/images/portfolio/woodcraft.jpg 1x, /images/portfolio/woodcraft@2x.jpg 2x" alt="">
+                        </a>
+                    </div>
+                    <div class="folio-item__info">
+                        <div class="folio-item__cat">Ecommerce Theme</div>
+                        <h4 class="folio-item__title">Digi</h4>
+                    </div>
+                    <a href="https://cameverett96.github.io/EcommerceTheme/" title="Project Link" class="folio-item__project-link">Project Link</a>
+                    <div class="folio-item__caption">
+                        <p>Theme built using html Css and Javascript</p>
+                    </div>
+                </div>
+            </div> <!-- end column -->
+            <!-- <div class="column" data-aos="fade-up">
+                <div class="folio-item">
+                    <div class="folio-item__thumb">
+                        <a class="folio-item__thumb-link" href="https://cameverett96.github.io/Shopify-next.js/" title="The Lamp" data-size="1050x700">
+                            <img src="/images/portfolio/lamp.jpg" 
+                                 srcset="/images/portfolio/lamp.jpg 1x, /images/portfolio/lamp@2x.jpg 2x" alt="">
+                        </a>
+                    </div>
+                    <div class="folio-item__info">
+                        <div class="folio-item__cat">Ecommerce Theme</div>
+                        <h4 class="folio-item__title">Stevi</h4>
+                    </div>
+                    <a href="https://cameverett96.github.io/Shopify-next.js/" title="Project Link" class="folio-item__project-link">Project Link</a>
+                    <div class="folio-item__caption">
+                        <p>Eccomerce theme built using Hmtl, Css, Next.js and Node.js</p>
+                    </div>
+                </div>
+            </div> end column -->
+
+        </div> <!-- folio-list -->
+
+    </section> <!-- end portfolio -->
+
+  
+ 
+    <?php require("../includes/scripts.php");?>
+   
+
+</body>
